5cee72c825928b712abe524d289a1efea6e4ecf4
DeepakGund <126459378+DeepakGund@users.noreply.github.com>
2024-11-26 15:39:55 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -53,6 +53,25 @@ resource "aws_instance" "ec2" {
   }
 }
 
+
+# Generate RSA private key
+resource "tls_private_key" "rsa_key" { 
+  algorithm = "RSA"
+}
+
+# Save the private key to a local file in PEM format
+resource "local_file" "pem_key_file" {
+  filename = "rsa_private_key.pem" # File will be created in the current directory
+  content  = tls_private_key.rsa_key.private_key_pem 
+}
+
+# (Optional) Output the private key for verification (avoid exposing sensitive data)
+output "private_key_pem" {
+  value     = tls_private_key.rsa_key.private_key_pem 
+  sensitive = true
+}
+
+
 output "Public_ips" {
   value = aws_instance.ec2.public_ip
 }
