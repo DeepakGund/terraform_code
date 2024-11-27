0eb802d38b82e4e7eadbf1a818c7cf0cbc0e5545
DeepakGund <126459378+DeepakGund@users.noreply.github.com>
2024-11-27 19:25:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,46 +1,47 @@
-  rsa_bits  = 4096  # Specify the key size (e.g., 4096 bits for a strong key)
+# Specify AWS provider version
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">=5.76.0"
+    }
+  }
 }
 
-# Save the private key to a local file in PEM format
-resource "local_file" "pem_key_file" {
-  filename = "deepak.pem"  # File will be created in the current directory with the new name
-  content  = tls_private_key.rsa_key.private_key_pem  # Correct resource reference
+# Local environment name
+locals {
+  env = "Terraform"
 }
 
-# (Optional) Output the private key for verification (avoid exposing sensitive data)
-output "private_key_pem" {
-  value     = tls_private_key.rsa_key.private_key_pem  # Correct resource reference
-  sensitive = true
+# Instance types variable
+variable "instance_types" {
+  type    = list(string)
+  default = ["t3.micro", "t3.medium", "t3.small"]
 }
 
-
-output "abc" {
-  value = aws_instance.ec2[1].public_ip
-}
-
-[root@ip-172-31-8-180 git]# vim main.tf
-
-[3]+  Stopped                 vim main.tf
-[root@ip-172-31-8-180 git]# ls
-deepak.pem  main.tf  terraform.tfstate  terraform.tfstate.backup
-[root@ip-172-31-8-180 git]# bash
-[root@ip-172-31-8-180 git]# vim main.tf
-tags = {
+# VPC
+resource "aws_vpc" "vpc" {
+  cidr_block = "10.10.0.0/16"
+  tags = {
     Name = "${local.env}-VPC"
   }
 }
 
-# vpc-subnet
+# Subnet
 resource "aws_subnet" "subnet" {
   vpc_id     = aws_vpc.vpc.id
   cidr_block = "10.10.10.0/24"
+  tags = {
+    Name = "${local.env}-Subnet"
   }
+}
 
-# EC2
+# EC2 Instances
 resource "aws_instance" "ec2" {
-  count         = length(var.instance_types)
+  count         = length(var.instance_types) # Create instances based on instance_types list
   subnet_id     = aws_subnet.subnet.id
   ami           = "ami-022b9b4e935404526"
+  key_name      = "us-west-02" # Updated key pair name
   instance_type = tolist(var.instance_types)[count.index]
   tags = {
     Name = "${local.env}-Server-${count.index + 1}"
@@ -50,51 +51,21 @@ resource "aws_instance" "ec2" {
 # Generate RSA private key
 resource "tls_private_key" "rsa_key" {
   algorithm = "RSA"
-  rsa_bits  = 4096  # Specify the key size (e.g., 4096 bits for a strong key)
+  rsa_bits  = 4096
 }
 
-# Save the private key to a local file in PEM format
+# Save RSA private key locally
 resource "local_file" "pem_key_file" {
-  filename = "deepak.pem"  # File will be created in the current directory with the new name
-  content  = tls_private_key.rsa_key.private_key_pem 
+  filename = "deepak.pem"
+  content  = tls_private_key.rsa_key.private_key_pem
 }
 
-# (Optional) Output the private key for verification (avoid exposing sensitive data)
+# Outputs
 output "private_key_pem" {
-  value     = tls_private_key.rsa_key.private_key_pem  
+  value     = tls_private_key.rsa_key.private_key_pem
   sensitive = true
 }
 
-
-/*
-# Create a new Key Pair in AWS
-resource "aws_key_pair" "key-gen" {
-  key_name   = "terraform_gen-key"   # Name of the key in AWS
-  public_key = tls_public_key.rsa_key.public_key_openssh  # public key
+output "public_ip_second_instance" {
+  value = aws_instance.ec2[1].public_ip # Outputs the public IP of the second instance
 }
-
-key_name      = aws_key_pair.key-gen.key_name 
-
-*/
-
-connection {
-type ="ssh"
-user = "ec2-user"
-private_key = file ("~/.ssh/us-west-o2.pem")
-
-provisioner "remote-exec" {
-inline [
-"sudo yum update -y"
-"sudo yum install git tree -y"
-"touch remote-exec.txt":wq
-
-]
-
-}
-
-}
-
-output "abc" {
-  value = aws_instance.ec2[1].public_ip
-}
-
